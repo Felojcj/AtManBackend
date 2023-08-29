fc0eb2ac4720d2e3133ad8562f25dbede4a20e8c
Felipe Correa <felocorrea18@gmail.com>
2023-08-29 15:34:18 -0500

    Get and Post Attendaces

---
+++ AtManBackend/Controllers/AttendancesController.cs
@@ -0,0 +1,33 @@
+﻿using AtManBackend.Data;
+using AtManBackend.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AtManBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AttendancesController: ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public AttendancesController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<List<Attendance>> Get()
+        {
+            return await context.Attendances.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Attendance attendance)
+        {
+            context.Add(attendance);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}

3a2853252d0783fdc7311e5561aa4871aacd258c
Felipe Correa <felocorrea18@gmail.com>
2023-08-30 15:23:49 -0500

    CRUD operations added

--- AtManBackend/Controllers/AttendancesController.cs
+++ AtManBackend/Controllers/AttendancesController.cs
@@ -1,4 +1,5 @@
 ﻿using AtManBackend.Data;
+using AtManBackend.DTO;
 using AtManBackend.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -22,12 +23,67 @@ namespace AtManBackend.Controllers
             return await context.Attendances.ToListAsync();
         }
 
-        [HttpPost]
-        public async Task<ActionResult> Post([FromBody] Attendance attendance)
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Attendance>> Get(int id)
         {
+            var attendance = await context.Attendances.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (attendance == null)
+            {
+                return NotFound($"The worker with the id {id} does not exist.");
+            }
+
+            return Ok(attendance);
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] AttendanceDTO attendanceDTO)
+        {
+            var attendance = new Attendance 
+            {
+                Name = attendanceDTO.Name,
+                StartDate = attendanceDTO.StartDate, 
+                EndDate = attendanceDTO.EndDate,
+            };
+
             context.Add(attendance);
             await context.SaveChangesAsync();
             return Ok();
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id, [FromBody] AttendanceDTO attendanceDTO)
+        {
+            var attendance = await context.Attendances.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (attendance == null)
+            {
+                return NotFound("The worker does not exists");
+            }
+
+            attendance.Name = attendanceDTO.Name;
+            attendance.StartDate = attendanceDTO.StartDate;
+            attendance.EndDate = attendanceDTO.EndDate;
+
+            context.Attendances.Update(attendance);
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var attendance = await context.Attendances.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(attendance == null) 
+            { 
+                return NotFound("The worker does not exists"); 
+            }
+
+            context.Attendances.Remove(attendance);
+            await context.SaveChangesAsync();
+            return NoContent();
         }
     }
 }
---
+++ AtManBackend/DTO/AttendanceDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AtManBackend.DTO
+{
+    public class AttendanceDTO
+    {
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+    }
+}
